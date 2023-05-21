e5fc2751571bf7852ed9388cb07e9a5031fdbc01
Paul McPhillips <87805886+PaulMcPhillips@users.noreply.github.com>
2023-05-21 02:03:38 +0100

    Add files via upload
    Changed imports to only import required libraries from PyQt6

--- Sorter.py
+++ Sorter.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt6.QtWidgets import *
+from PyQt6.QtWidgets import QWidget, QGridLayout, QComboBox, QLabel, QPushButton, QLineEdit, QFileDialog, QApplication
 import pathlib
 import os
 import shutil
